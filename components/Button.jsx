2fcb2f5f0e61cc38957e0b84ba97c9913771060e
pevescarol <carol.peves@gmail.com>
2023-10-19 16:49:56 -0300

    cambios config gral

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Button = ({text, variant}) => {
   return (
-    <button  className={`rounded-full border ${variant}`}>
+    <button  className={`rounded-full ${variant}`}>
       <Link href='/' className='bold-16 whitespace-nowrap cursor-pointer uppercase'>{text}</Link>
     </button>
   )
