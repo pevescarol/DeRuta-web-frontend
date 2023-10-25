abcb9d5739450edc3bf7b8b0c288e125ddeab0f6
pevescarol <carol.peves@gmail.com>
2023-10-25 19:50:01 -0300

    se modifica h2

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -5,15 +5,15 @@ import {FaArrowRight} from 'react-icons/fa'
 const Blog = () => {
   return (
     <section className='2xl:mx-auto 2xl:max-w-[1440px] relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
-      <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
+      <div className='hide-scrollbar flex h-[500px] w-full items-start justify-start gap-8 overflow-x-auto xl:h-[640px]'>
         <BlogItem backgroundImage='bg-blog-item' title='Parque Nacional Ciervo de los Pantanos' text='Otamendi, Campana' />
         <BlogItem backgroundImage='bg-blog-item-2' title='Areco Camping' text='San Antonio de Areco' />
       </div>
 
       <div className='flex items-center justify-end mt-10 px-6 lg:-mt-60 lg:mr-6'>
-        <div className='bg-green-600 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-[30px] xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-xl '>
-          <h2 className='regular-24 md:regular-32 2xl:regular-64 capitalize text-white'>
-            <strong>¿No sabes a dónde ir?</strong> <br />Descubre las experiencias de otros ciclistas.
+        <div className='bg-green-600 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-[40px] '>
+          <h2 className='regular-24 text-white'>
+            <strong>¿Vas a ir a un sitio donde nunca has estado y quieres dar una vuelta por allí? 👀</strong>
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">
             La comunidad comparte sus experiencias, destinos, que rutas seguir, consejos, etc.
